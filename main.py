bfad19bff59b68e2a34f9be2096cbdd7b4a05b94
Rodion Fastunov <112808867+KaFFa0@users.noreply.github.com>
2025-03-04 15:23:14 +0300

    add sliders

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import streamlit
+import streamlit as st
 import requests
 
-streamlit.title("Хостинг-провайдер")
-type = streamlit.selectbox("Тип", ["Виртуалка", "Контейнер"])
-resourse = ''
-if type == "Виртуалка":
-    resourse = streamlit.selectbox("ОС для виртуалки", ["Ubuntu", "Fedora"])
-else:  # Контейнер
-    resourse = streamlit.selectbox("Образ для контейнера", ["Nginx", "Apache"])
+st.title("Хостинг-провайдер")
+type = st.selectbox(
+    "Тип",
+    ["Виртуалка", "Контейнер"]
+)
 
-if streamlit.button("Создать"):
-    payload = {"type": type, "resource": resourse}
+if type == "Виртуалка":
+    resource = st.selectbox("ОС для виртуалки", ["Ubuntu", "Fedora"])
+else:  # Контейнер
+    resource = st.selectbox("Образ для контейнера", ["Nginx", "Apache"])
+
+ram = st.slider("Объём RAM (ГБ)", 1, 32, 4)
+cpu = st.slider("Количество vCPU", 1, 16, 2)
+disk = st.slider("Дисковое пространство (ГБ)", 10, 100, 15)
+
+if st.button("Создать"):
+    # payload = {"type": type, "resource": resourse}
     #response = requests.post("http://localhost:8000/create", json=payload)
     #st.write(response.json()["details"])
+    pass
